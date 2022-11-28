05504754a9997fa47565f7e83dd1f2f068da0469
pshigotskaya26 <pshigotskaya.tanya@tut.by>
2022-11-28 11:38:27 +0300

    fix: change structure of the project

--- js/script.js
+++ js/script.js
@@ -2,39 +2,40 @@ const burgerNode = document.querySelector('.burger');
 const burgerCloseNode = document.querySelector('.burger-close');
 const bodyHiddenNode = document.querySelector('.body-hidden');
 const bodyNode = document.querySelector('.body');
-const headerNavigationNode = document.querySelector('.header-navigation');
+const headerNavigationNode = document.querySelector('.header__navigation');
 const navigationItemLinks = document.querySelectorAll('.header-navigation__item a');
 
 burgerNode.addEventListener('click', function() {
-    burgerNode.classList.toggle('hidden');
-    burgerCloseNode.classList.toggle('hidden');
-    bodyHiddenNode.classList.toggle('hidden');
-    bodyNode.classList.toggle('lock');
+    burgerNode.classList.add('hidden');
+    burgerCloseNode.classList.remove('hidden');
+    bodyHiddenNode.classList.remove('hidden');
+    bodyNode.classList.add('lock');
     headerNavigationNode.style.height = "330px";
 });
 
 burgerCloseNode.addEventListener('click', function() {
-    burgerNode.classList.toggle('hidden');
-    burgerCloseNode.classList.toggle('hidden');
-    bodyHiddenNode.classList.toggle('hidden');
-    bodyNode.classList.toggle('lock');
+    burgerNode.classList.remove('hidden');
+    burgerCloseNode.classList.add('hidden');
+    bodyHiddenNode.classList.add('hidden');
+    bodyNode.classList.remove('lock');
     headerNavigationNode.style.height = "0px";
 });
 
 bodyHiddenNode.addEventListener('click', function() {
-    burgerNode.classList.toggle('hidden');
-    burgerCloseNode.classList.toggle('hidden');
-    bodyHiddenNode.classList.toggle('hidden');
-    bodyNode.classList.toggle('lock');
+    burgerNode.classList.remove('hidden');
+    burgerCloseNode.classList.add('hidden');
+    bodyHiddenNode.classList.add('hidden');
+    bodyNode.classList.remove('lock');
     headerNavigationNode.style.height = "0px";
 });
 
 navigationItemLinks.forEach(item => {
-    item.addEventListener('click', function() {
-        burgerNode.classList.toggle('hidden');
-        burgerCloseNode.classList.toggle('hidden');
-        bodyHiddenNode.classList.toggle('hidden');
-        bodyNode.classList.toggle('lock');
-        headerNavigationNode.style.height = "0px";
-    });
+	item.addEventListener('click', function() {
+		burgerNode.classList.remove('hidden');
+		burgerCloseNode.classList.add('hidden');
+		bodyHiddenNode.classList.add('hidden');
+		bodyNode.classList.remove('lock');
+		headerNavigationNode.style.height = "0px";
+	});
 });
+
